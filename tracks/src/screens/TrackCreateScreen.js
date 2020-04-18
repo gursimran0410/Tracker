a9fbbe3236e2054ef7cd6da15e6d711261f805fd
gursimran0410 <gursimransingh154@gmail.com>
2020-04-18 21:51:49 +0530

    Initialization of different screens

---
+++ tracks/src/screens/TrackCreateScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, StyleSheet, Text} from 'react-native'
+
+
+const TrackcreateScreen = () => {
+    return <Text style={{fontSize:48}}>Track Create Screen</Text>
+}
+
+const styles = StyleSheet.create({})
+
+export default TrackcreateScreen
